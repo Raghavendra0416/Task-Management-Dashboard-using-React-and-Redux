5ce896b85483d9ca6fe7529b41c551539168bb62
Raghavendra0416 <raghavendra.workin@gmail.com>
2026-04-10 22:29:02 +0530

    Created Other Folder

---
+++ Other/Components/NavBar.jsx
@@ -0,0 +1,16 @@
+import tasksImg from '../assets/tasksImg.svg'
+import '../CSS/NavStyle.css'
+
+export default function NavBar() {
+    return (
+        <div id="NavBar">
+            <div>
+                <img src={tasksImg} alt="TasksImg" id="logo" />
+            </div>
+            <div>
+                <h3>TASK MANAGEMENT DASHBOARD</h3>
+                <p>Organize and track your tasks</p>
+            </div>
+        </div>
+    )
+}
